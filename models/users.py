b2eea25ac114b951fb5bf30e968a96b24408e36a
StructuredTrading <samowen1992@hotmail.com>
2024-02-10 10:36:37 +1000

    Create CLI controller

--- models/users.py
+++ models/users.py
@@ -6,12 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String,)
     email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False, minlength=6)
+    password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 class UserSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'email', 'password', 'is_admin')
 
-user_schema = UserSchema(excludes=['password'])
-users_schema = UserSchema(many=True, excludes=['password'])
+user_schema = UserSchema(exclude=['password'])
+users_schema = UserSchema(many=True, exclude=['password'])
